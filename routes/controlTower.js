4f880b47cf52095c09ec275b0252bfbd196d8a45
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-13 23:21:21 +0500

    fix: handle cors wildcard configurations specifically for local web-sockets

--- routes/controlTower.js
+++ routes/controlTower.js
@@ -6,7 +6,7 @@ const { getPredictiveInsights } = require('../services/aiEngine');
 // GET: Aggregated  
 router.get('/dashboard-summary', async (req, res) => {
   try {
-    // MongoDB Aggregation  ka  
+    // MongoDB   ka  
     const stats = await Inventory.aggregate([
       {
         $group: {
