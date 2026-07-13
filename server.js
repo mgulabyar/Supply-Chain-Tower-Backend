69ce98f92e76bbdcf8df253c05fd13c4a2881613
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-13 23:15:26 +0500

    feat: configure fine-tuned gpt-4o parameters for low stock insights

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 // require('dotenv').config();
 // const express = require('express');
 // const mongoose = require('mongoose');
-// const http = require('http');
-// const { Server } = require('socket.io');
 
 
 
