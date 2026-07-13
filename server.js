e2a4126babc11f95d28600128b773b69cb65bd21
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-13 23:14:01 +0500

    feat: build data seeding api route for testing dummy inventory

--- server.js
+++ server.js
@@ -10,10 +10,6 @@
 // const server = http.createServer(app);
 // const io = new Server(server, { cors: { origin: "*" } });
 
-// app.use(cors());
-// app.use(express.json());
-// app.use('/api/tower', controlRoutes);
-
 
 
 require('dotenv').config();
